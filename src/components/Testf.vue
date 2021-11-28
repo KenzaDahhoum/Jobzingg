2195262efa52214c192316eb73db45dec7e752af
dahhou <dahhoumkenza@gmail.com>
2021-11-28 08:16:35 +0100

    table + link

---
+++ src/components/Testf.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="tabs">
+        <v-div class="container">
+            <v-div class="slider">
+         
+            <v-btn  flat text  to="/Folder" depressed  class="ml-2 mt-0">
+         <v-icon left color="#1976d2" small >mdi-heart</v-icon>
+         <span class="p-3">Folder</span> 
+         </v-btn>
+           
+            </v-div>
+  
+        </v-div>
+      
+    </div>
+</template>
+<style>
+
+</style>
